7994e13820b9b69eb5bda28ae5be7736cc961f4b
mmccurdy07 <mmccurdy@gmail.com>
2014-08-10 12:59:41 -0700

    Make sure the summary is properly unescaped (use the Framework HTML parser).  Fixes #1.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -145,7 +145,7 @@ class WikipediaAgent(Agent.Movies):
     
       # FIXME, need to resolve these: -{zh-hans:港译《侠盗·骄雄》; zh-hant:香港譯《俠盜·驕雄》; zh-hk:中國大陸及台灣均譯《羅賓漢》;}-
     
-      summary = String.StripTags(summary).replace('&nbsp;',' ').replace('  ',' ').replace(' ,', ',').strip()
+      summary = String.StripTags(HTML.StringFromElement(HTML.ElementFromString(summary)))
       metadata.summary = summary
       
     except:
